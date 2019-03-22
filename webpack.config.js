ab2223436ded3616cc73b9cbf317f1116ea5abbe
Wolfgang Schuster <wolfgang.schuster@fourwindsinteractive.com>
2019-03-22 15:55:57 -0600

    Clean up building

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,46 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: path.join(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        exclude: /node_modules|elm-stuff/,
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+module.exports = ({ prod } = {}) => {
+  return {
+    mode: prod ? 'production' : 'development',
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          exclude: /node_modules|elm-stuff/,
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-      {
-        exclude: /node_modules|elm-stuff/,
-        test: /\.elm$/,
-        use: 'elm-webpack-loader',
-      },
+        {
+          exclude: /node_modules|elm-stuff/,
+          test: /\.elm$/,
+          use: {
+            loader: 'elm-webpack-loader',
+            options: { optimize: !!prod },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'src/index.html' }),
+      new CopyPlugin([
+        { from: 'src/assets', to: 'assets' },
+        { from: 'src/manifest.json', to: '' },
+      ]),
     ],
-  },
-  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
-  devServer: {
-    port: 8000,
-  },
+    devServer: {
+      port: 8000,
+    },
+  };
 };
